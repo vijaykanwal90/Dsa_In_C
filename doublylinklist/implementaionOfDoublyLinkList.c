326abeee65c1487c697415e10c7aef9d4813c9ac
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-03-16 17:49:49 +0530

    writting add node function

--- doublylinklist/implementaionOfDoublyLinkList.c
+++ doublylinklist/implementaionOfDoublyLinkList.c
@@ -1,5 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+typedef struct  node{
+    struct node * prev;
+    int data;
+    struct node * next;
+} node_type;
+void addNode(node_type **left, node_type **right){
+    node_type *p;
+    p = (node_type *) malloc(sizeof(node_type));
+    if(p!=NULL){
+        if(right==NULL){
+            
+        }
+    }
+    else {
+        printf("not enough memory\n");
+    }
+}
 int main(){
+    node_type * left;
+    node_type * right;
+    right = left = NULL;
+    int option ;
+    do
+    {
+        printf("1 to add \n");
+        printf("2 to add \n");
+        printf("3 to add \n");
+        printf("4 to add \n");
+        scanf("%d",&option);
+        switch (option)
+        {
+        case 1:
+            addNode(&left, &right);
+            break;
+        case 2:
+            deleteNode(&left, &right);
+            break;
+        case 3:
+            displayNode(left, right);
+            break;
+        case 4:
+            printf("exit successfully\n");
+            break;
+        default:
+            break;
+        }
+
+    } while (option!=4);
+    
     return 0;
 }
