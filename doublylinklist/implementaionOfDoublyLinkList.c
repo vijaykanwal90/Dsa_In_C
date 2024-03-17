be2ac05bb764bdba95f50eab351c798dd0e512a8
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-03-17 11:01:46 +0530

    implementation of doubly link list and printing from left to right and right to left

--- doublylinklist/implementaionOfDoublyLinkList.c
+++ doublylinklist/implementaionOfDoublyLinkList.c
@@ -1,45 +1,84 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct  node{
-    struct node * prev;
+typedef struct node
+{
+    struct node *prev;
     int data;
-    struct node * next;
+    struct node *next;
 } node_type;
-void addNode(node_type **left, node_type **right){
+void addNode(node_type **left, node_type **right)
+{
     node_type *p;
-    p = (node_type *) malloc(sizeof(node_type));
-    if(p!=NULL){
-        if(right==NULL){
-            
+    p = (node_type *)malloc(sizeof(node_type));
+    if (p != NULL)
+    {
+        printf("Enter the data\n");
+        scanf("%d", &p->data);
+        p->next = NULL;
+        if ((*right) == NULL && (*right) == NULL)
+        {
+            (*left) = p;
+            (*right) = p;
+            (*left)->prev = NULL;
+        }
+        else
+        {
+
+            (*right)->next = p;
+            p->prev = (*right);
+            (*right) = p;
         }
     }
-    else {
+    else
+    {
         printf("not enough memory\n");
     }
 }
-int main(){
-    node_type * left;
-    node_type * right;
+void leftToRight(node_type *left)
+{
+    node_type *temp = left;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+void rightToLeft(node_type *right)
+{
+    node_type *temp = right;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->prev;
+    }
+    printf("\n");
+}
+int main()
+{
+    node_type *left;
+    node_type *right;
     right = left = NULL;
-    int option ;
+    int option;
     do
     {
         printf("1 to add \n");
-        printf("2 to add \n");
-        printf("3 to add \n");
-        printf("4 to add \n");
-        scanf("%d",&option);
+        printf("2 to display left to right \n");
+        printf("3 to display right to left \n");
+        printf("4 to exit \n");
+        scanf("%d", &option);
         switch (option)
         {
         case 1:
             addNode(&left, &right);
             break;
         case 2:
-            deleteNode(&left, &right);
+            leftToRight(left);
             break;
         case 3:
-            displayNode(left, right);
+            rightToLeft(right);
+
             break;
         case 4:
             printf("exit successfully\n");
@@ -48,7 +87,7 @@ int main(){
             break;
         }
 
-    } while (option!=4);
-    
+    } while (option != 4);
+
     return 0;
 }
